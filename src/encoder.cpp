1fce2df8618fde1082ea229a5ae06aa81173ee21
yistLin <yishen992@gmail.com>
2017-06-18 18:46:56 +0800

    the very beginning of encoding process

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 
 #include "log.h"
+#include "util.h"
+#include "io.h"
+#include "frame.h"
 
-int main(int argc, const char *argv[])
-{
+void encode_sequence(Reader& reader) {
+  int curr_frame = 0;
+
+  while (curr_frame < reader.nb_frames) {
+    PadFrame pf = reader.get_padded_frame();
+    Frame frame(pf);
+
+    curr_frame++;
+  }
+}
+
+int main(int argc, const char *argv[]) {
   Log logger("Main");
-  logger.log(Level::ERROR, "ker");
+
+  // Get command-line arguments
+  Util util(argc, argv);
+
+  // Read from given filename
+  Reader reader(util.input_file, util.width, util.height);
+
+  // Encoding process start
+  encode_sequence(reader);
+
   return EXIT_SUCCESS;
 }
+
