c621c159059a71ea763d82fcc8f549dc810de94a
Yist Lin <yishen992@gmail.com>
2017-06-18 17:31:06 +0800

    use int type for negative number

--- inc/block.h
+++ inc/block.h
@@ -2,10 +2,9 @@
 #define BLOCK
 
 #include <array>
-#include <cstdint>
 
 #define PIXELS_PER_BLOCK 8*8
 
-typedef std::array<std::uint8_t, PIXELS_PER_BLOCK> Block;
+typedef std::array<int, PIXELS_PER_BLOCK> Block;
 
 #endif
