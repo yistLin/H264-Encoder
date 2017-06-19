f8565c50991d9640a163f50b9e1a1524d5fd80f3
JCly-rikiu <jcly.rikiu@gmail.com>
2017-06-19 16:06:01 +0800

    change typedef to using for template

--- inc/block.h
+++ inc/block.h
@@ -5,7 +5,7 @@
 
 #define PIXELS_PER_BLOCK 8*8
 
-typedef std::array<int, 8*8> Block8x8;
-typedef std::array<int, 16*16> Block16x16;
+using Block8x8 = std::array<int, 8*8>;
+using Block16x16 = std::array<int, 16*16>;
 
 #endif
