ac5ad2b552473a0e6bac79f3b4b19c06f0b3c705
yistLin <yishen992@gmail.com>
2017-06-25 00:14:04 +0800

    tested vlc on block4x4

--- inc/vlc.h
+++ inc/vlc.h
@@ -6,9 +6,12 @@
 #include <string>
 #include <cstdint>
 
+#include "block.h"
 #include "bitstream.h"
 
 Bitstream exp_golomb(const int);
 Bitstream signed_exp_golomb(const int);
+
+Bitstream cavlc_block4x4(Block4x4, const int);
 
 #endif
