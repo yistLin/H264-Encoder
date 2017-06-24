d85fd72493bc8b9cec9e65bbe212d7c4f653c283
WindQAQ <b03902042@ntu.edu.tw>
2017-06-25 05:53:47 +0800

    add me table

--- inc/vlc.h
+++ inc/vlc.h
@@ -9,6 +9,19 @@
 #include "block.h"
 #include "bitstream.h"
 
+const int me[] = {
+	3 , 29, 30, 17, 31,
+	18, 37, 8 , 32, 38, 
+	19, 9 , 20, 10, 11, 
+	2 , 16, 33, 34, 21, 
+	35, 22, 39, 4 , 36, 
+	40, 23, 5 , 24, 6 , 
+	7 , 1 , 41, 42, 43, 
+	25, 44, 26, 46, 12, 
+	45, 47, 27, 13, 28, 
+	14, 15, 0
+};
+
 Bitstream ue(const unsigned int);
 Bitstream se(const int);
 
